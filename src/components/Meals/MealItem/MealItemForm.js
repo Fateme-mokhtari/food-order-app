1dc456f93ed006e8bd028b3f7bb24a6142569268
Fatemeh <fatememokhtari.7020@gmail.com>
2021-11-23 11:03:31 +0100

    add meta item meal and fix metaitem bug

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,4 +1,19 @@
-const MealItemForm=()=>{
-    return
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
+
+const MealItemForm=(props)=>{
+    return(
+        <div className={classes.form}>
+            <Input label="amount" input={{
+                id: 'amount_' + props.id,
+                type:"number",
+                min:"1",
+                max:"5",
+                step:"1",
+                defaultValue:"1"
+            }}/>
+            <button>+ add</button>
+        </div>
+    )
 }
 export default MealItemForm
