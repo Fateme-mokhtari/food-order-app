322f9ce89e837a6d0f724dc3f574ecfd52f81e67
Fatemeh <fatememokhtari.7020@gmail.com>
2021-11-24 16:29:23 +0100

    update cat component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,22 +1,36 @@
+import { useContext } from "react";
+import CartContext from "../../sore/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
-const cartItem=<ul className={classes['cart-items']}>
-{[{id:'c1',name:'Sushi',amount:'2',price:12.99}].map(item=><li>{item.name}</li>)}
-</ul>
+    
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItem = cartCtx.items.length > 0;
 
-  return <Modal onClose={props.onClose}>
+  const cartItem = (
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
       {cartItem}
       <div className={classes.total}>
-          <span>Total amount</span>
-          <span>35.66</span>
+        <span>Total amount</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-          <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-          <button className={classes.button}>Order</button>
-
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItem && <button className={classes.button}>Order</button>}
       </div>
-  </Modal>;
+    </Modal>
+  );
 };
 export default Cart;
